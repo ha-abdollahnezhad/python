ceca4779ec403133ce5893ca2e6280f0ea1a2b36
hamed <ha.abdollahnezhad@gmail.com>
2026-01-06 14:18:32 +0330

    add for loop some parts

--- test.py
+++ test.py
@@ -1553,3 +1553,61 @@ print("-------------------")
 # Note: The else block will NOT be executed
 # if the loop is stopped by a break statement.
 
+# 28- Python For Loops
+fruits = ['apple', 'banana', 'cherry', 'mango', 'melon']
+for x in fruits :
+    print(x)
+print("-------------------")
+
+for x in 'banana':
+    print(x)
+print("-------------------")
+
+for x in fruits:
+    if x == 'banana':
+        break
+    print(x)
+print("-------------------")
+
+for x in fruits:
+    if x == 'cherry':
+        continue
+    print(x)
+print("-------------------")
+
+for x in range(6):
+    print(x)
+print("-------------------")
+
+for x in range(2, 6):
+    print(x)
+print("-------------------")
+
+for x in range(2, 30, 3):
+    print(x)
+print("-------------------")
+
+for x in range(6):
+    print(x)
+else:
+    print('finally finished')
+print("-------------------")
+
+for x in range(6):
+    if x == 3: break
+    print(x)
+else:
+    print('finally finished!')
+print("-------------------")
+
+adj = ['red', 'big', 'tasty']
+fruits = ['apple', 'banana', 'cherry']
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
+print("-------------------")
+
+for x in [0, 1, 2]:
+    pass
+print("-------------------")
