4c4fff42035ff5f6a80c22004c90f0d4de1db098
hamed <ha.abdollahnezhad@gmail.com>
2026-01-28 15:02:07 +0330

    add class methods, inheritance sections

--- python_classes_documentation.py
+++ python_classes_documentation.py
@@ -512,3 +512,160 @@ del Person.greet
 # p1.greet() This will cause an error
 print("-------------------")
 
+# 6- Python Inheritance
+'''
+Inheritance allows us to define a class that inherits all the 
+methods and properties from another class.
+
+Parent class is the class being inherited from, also called
+base class.
+
+Child class is the class that inherits from another class, also
+called derived class.
+'''
+
+# Create a Parent Class
+'''
+Any class can be a parent class, so the syntax is the same as
+creating any other class:
+Create a class named Person, with firstname and lastname 
+properties, and a printname method:
+'''
+
+class Person:
+    def __init__(self, firstName, lastName):
+        self.firstName = firstName
+        self.lastName = lastName
+
+    def print_name(self):
+        print(self.firstName, self.lastName)
+
+x = Person('Ali', 'Mohammadi')
+x.print_name()
+print("-------------------")
+
+# Create a Child Class
+'''
+To create a class that inherits the functionality from another
+class, send the parent class as a parameter when creating the 
+child class:
+
+Create a class named Student, which will inherit the properties
+and methods from the Person class:
+'''
+
+class Student(Person):
+    pass
+
+'''
+Note: Use the pass keyword when you do not want to add any other
+properties or methods to the class.
+'''
+
+x = Student('Hamed', 'Alavi')
+x.print_name()
+print("-------------------")
+
+# Add the __init__() Function
+'''
+So far have create a child class that inherites the properties
+and methods from its parent.
+We want to add the __init__() function to the child class
+(instead of the pass keyword).
+
+Note: The __init__() function is called automatically every time
+the class is being used to create a new object.
+'''
+
+class Student(Person):
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def print_student_name(self):
+        print('Student : ',self.fname, self.lname)
+
+x = Student('Hamed', 'Alavi')
+x.print_student_name()
+print("-------------------")
+
+'''
+When you add the __init__() function, the child class will no
+longer inherit the parent's __init__() function.
+
+Note: The child's __init__() function overrides the inheritance
+of the parent's __init__() function.
+
+To Keep the inheritance of the parent's __init__() function, add
+a call to the parent's __init__() function:
+'''
+class Student(Person):
+    def __init__(self, fname, lname):
+        Person.__init(self, fname, lname)
+
+'''
+Now we have successfully added the __init__() function,
+and kept the inheritance of the parent class, and we are ready
+to add functionality in the __init__() function.
+'''
+
+# Use the super() Function
+'''
+Python also has a super() function that will make the child class
+inherit all the methods and properties from its parent:
+'''
+
+class Student(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+
+'''
+By using the super() function, you do not have to use the name
+of the parent element,
+it will automatically inherit the methods and properties from 
+its parent.
+Add a property called graduationYear to the Student class:
+'''
+
+class Student(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+        self.graduationYear = 2020
+
+'''
+In the example below, the year 2020 should be a variable, and
+passed into the Student class when creating student objects. To
+do so, add another parameter in the __init__() function:
+'''
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationYear = year
+x = Student('Hamed', 'Alavi', 2020)
+
+# Add Methods
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationYear = year
+
+    def welcome(self):
+        print('Welcome', self.firstName, self.lastName, 'to the class of', self.graduationYear)
+
+x = Student('Hamed', 'Abdollah', 2020)
+x.welcome()
+print("-------------------")
+
+'''
+If you add a method in the child class with the same name as a
+function in the parent class, the inheritance of the parent
+method will be overridden.
+'''
+
+# 6- Python Polymorphism
+'''
+The word 'polymorphism' means 'many forms', and in programming
+it refers to me
+'''
