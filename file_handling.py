1666f4edbe43d75e6b3fda0832a392956b2ac704
hamed <ha.abdollahnezha@gmail.com>
2026-01-29 22:53:20 +0330

    add several part of file handling

--- file_handling.py
+++ file_handling.py
@@ -36,3 +36,80 @@ Because "r" for read, and "t" for text are the default values,
 you do not need to specify them.
 Note: Make sure the file exists, or else you will get an error.
 '''
+
+# Open a File on the Server
+# Assume we have the following file, located in the same folder as Python:
+# To open the file, use the built-in open() function.
+# The open() function returns a file object, which has a read() method
+# for reading the content of the file:
+
+f = open('demofile.txt')
+print(f.read())
+print("-------------------")
+
+# If the file is located in a different location, you will have to 
+# specify the file path, like this:
+
+f = open("C:\\Users\haabd\python\demofile.txt")
+print(f.read())
+print("-------------------")
+
+# Using the with statement
+# You can also use the with statement when opening a file:
+
+with open('demofile.txt') as f:
+    print(f.read())
+print("-------------------")
+# Then you do not have to worry about closing your files, the with statement
+# takes care of that.
+
+# Close Files
+'''
+It is a good practice to always close the file when you are done with it.
+
+If you are not using the with statement, you must write a close statement
+in order to close the file:
+'''
+f = open('demofile.txt')
+print(f.readline())
+f.close()
+print("-------------------")
+
+'''
+Note: You should always close your files. In some cases, due to buffering,
+changes made to a file may not show until you close the file.
+'''
+
+# Read Only Parts of the File
+'''
+By default the read() method returns the whole text. but you can also
+specify how many characters you want to return:
+'''
+with open('demofile.txt') as f:
+    print(f.read(5))
+print("-------------------")
+
+# Read Lines
+'''
+You can return one line by using the readline() method:
+'''
+
+with open('demofile.txt') as f:
+    print(f.readline())
+print("-------------------")
+
+# By calling readline() two times, you can read the two first lines:
+
+with open('demofile.txt') as f:
+    print(f.readline())
+    print(f.readline())
+print("-------------------")
+
+# By looping through the lines of the file, you can read the whole file,
+# line by line:
+
+with open('demofile.txt') as f:
+    for x in f:
+        print(x)
+print("-------------------")
+
